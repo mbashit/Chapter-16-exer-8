dded7887cba02d109d8339fba5e4079231f3b5a4
Dell <Dell@DESKTOP-SII957I>
2022-09-23 22:31:21 +0300

    Add project files.

---
+++ Chapter 16 exer 8/Chapter 16 exer 8.cpp
@@ -0,0 +1,26 @@
+// Chapter 16 exer 8.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+/*Write a C++ program that prompts the user to enter a string, and then
+displays the message “Uppercase” when the given string contains only
+uppercase characters*/
+
+#include <iostream>
+#include <string>
+#include<boost/algorithm/string.hpp>
+using namespace boost::algorithm;
+
+using namespace std;
+
+
+int main()
+{
+    string  word;
+
+    cout << "enter the string";
+    cin >> word;
+
+    if (to_upper_copy(word)) {
+        cout << "Uppercase" << endl;
+    }
+}
+
